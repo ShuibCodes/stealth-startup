f200556561dc63dbda10a34f330756d38b4fd075
Moe_azaza <mohamed@ranadev.io>
2025-03-08 18:09:37 +0100

    change firebase project

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlk-GnkePXjy0jd7gnoRnuNprJ9-z4AjE",
-  authDomain: "codingkids-ef37b.firebaseapp.com",
-  projectId: "codingkids-ef37b",
-  storageBucket: "codingkids-ef37b.firebasestorage.app",
-  messagingSenderId: "646131079419",
-  appId: "1:646131079419:web:38667cd076e376298cf85e",
+  apiKey: "AIzaSyDERcVBKOMQQJRjdBQqBq9Zkwpy855PfGo",
+  authDomain: "codingkids-91234.firebaseapp.com",
+  projectId: "codingkids-91234",
+  storageBucket: "codingkids-91234.firebasestorage.app",
+  messagingSenderId: "785106443306",
+  appId: "1:785106443306:web:cdf667370f5a37cdda3753",
+  measurementId: "G-45DE9FB4P8",
 };
 
 // Initialize Firebase
