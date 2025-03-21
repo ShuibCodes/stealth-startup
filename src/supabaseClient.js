a7ed92e4e4b928c1b0053e95ada91d9e53801e2d
Moe_azaza <mohamed@ranadev.io>
2025-03-21 22:56:22 +0100

    move to new supabase project

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://plbuzznwhpvbcdiyiqmg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsYnV6em53aHB2YmNkaXlpcW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MDYzMzgsImV4cCI6MjA1NzI4MjMzOH0.EGcTZBPaJkyRtAHQqvw_SAB0ohoouQ4ajgZAT-jhEhI"
+  "https://uwfrsmcfvqmbxaxxdrgh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3ZnJzbWNmdnFtYnhheHhkcmdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1OTA2MzAsImV4cCI6MjA1ODE2NjYzMH0.WfnXWgiWaF7eNcQMNsOCy7v91xZPfQGtiQ2efj4-CS0"
 );
 
 export default supabase;
